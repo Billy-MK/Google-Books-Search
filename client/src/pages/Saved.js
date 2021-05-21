a020ce8df57f6559326614aee428ccde8d383d56
Billy Keating <wmontoothkeating@gmail.com>
2021-05-20 17:31:23 -0700

    Began React with two pages, currently just displaying text elements

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+function Saved() {
+    return <div>Saved Page</div>
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+function Search() {
+    return <div>Search Page</div>
+}
+
+export default Search;
